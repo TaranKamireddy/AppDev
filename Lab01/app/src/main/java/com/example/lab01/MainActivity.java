ced414dfc6b85a8808189f78939bfd3e4ced91f7
TaranKamireddy <tarankamireddy@gmail.com>
2023-08-24 10:16:08 -0700

    added counter

--- Lab01/app/src/main/java/com/example/lab01/MainActivity.java
+++ Lab01/app/src/main/java/com/example/lab01/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.lab01;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -10,22 +11,41 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
     Button incrementButton;
     Button decrementButton;
+    Button startButton;
     TextView titleDisplay;
-    int count=0;
+    int count = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         incrementButton = findViewById(R.id.increment_button);
         decrementButton = findViewById(R.id.decrement_button);
+        startButton = findViewById(R.id.start_button);
         titleDisplay = findViewById(R.id.heading);
+        startButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v) {
+                new CountDownTimer(count, 1000){
+                    public void onTick(long millisUntilFinished){
+                        titleDisplay.setText(String.valueOf(count/1000));
+                        count -= 1000;
+                    }
+                    public void onFinish(){
+                        titleDisplay.setText("FINISH!!");
+                    }
+                }.start();
+            }
+        });
     }
     public void increment(View view) {
-        System.out.println("incrementing: "+ ++count);
-        titleDisplay.setText(""+count);
+        count += 1000;
+        System.out.println("incrementing: "+ count);
+        titleDisplay.setText(""+count/1000);
     }
     public void decrement(View view) {
-        System.out.println("decrementing: "+ --count);
-        titleDisplay.setText(""+count);
+        count -= 1000;
+        System.out.println("decrementing: "+ count);
+        titleDisplay.setText(""+count/1000);
     }
 }
