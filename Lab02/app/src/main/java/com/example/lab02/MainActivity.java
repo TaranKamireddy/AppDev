334e68a32998c21dc70b9ff0e9905765109539a4
TaranKamireddy <tarankamireddy@gmail.com>
2023-08-31 09:16:51 -0700

    lab02

--- Lab02/app/src/main/java/com/example/lab02/MainActivity.java
+++ Lab02/app/src/main/java/com/example/lab02/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.lab02;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
+import android.graphics.LinearGradient;
+import android.graphics.Shader;
 import android.os.Bundle;
+import android.text.TextPaint;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -27,6 +31,17 @@ public class MainActivity extends AppCompatActivity {
         messageDisplay = findViewById(R.id.message);
         messageDisplay.setText(getString(R.string.first_message,messages[0],count));
         setName = findViewById(R.id.name);
+        TextPaint paint = messageDisplay.getPaint();
+        Shader textShader = new LinearGradient(0, 0, 220, messageDisplay.getTextSize(),
+                new int[]{
+                        Color.parseColor("#F97C3C"),
+                        Color.parseColor("#FDB54E"),
+                        Color.parseColor("#64B678"),
+                        Color.parseColor("#478AEA"),
+                        Color.parseColor("#8446CC"),
+                }, null, Shader.TileMode.CLAMP);
+        titleDisplay.getPaint().setShader(textShader);
+        messageDisplay.getPaint().setShader(textShader);
     }
     public void increment(View view) {
         System.out.println("incrementing: "+ count);
